689b826e978665a5fae2be192380104618faba98
rinstance <rinat.nur.93@mail.ru>
2020-09-20 18:22:04 +0300

    Add homework-2

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,17 +2,31 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    var isEdit: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val bmw = BMW("X5", 2)
-        val lada = Lada("Granta", 4)
-        if (lada.isSportCar)
-            println("Lada is sport car")
-        else
-            println("BMW is awesome")
+        button.setOnClickListener { edit() }
+    }
+
+    private fun edit() {
+        if (!isEdit) {
+            editText.visibility = View.VISIBLE
+            textView.visibility = View.INVISIBLE
+            editText.setText(textView.text)
+            button.text = "Сохранить"
+        } else {
+            textView.text = editText.text
+            editText.visibility = View.INVISIBLE
+            textView.visibility = View.VISIBLE
+            button.text = "Редактировать"
+        }
+        isEdit = !isEdit
     }
 }
