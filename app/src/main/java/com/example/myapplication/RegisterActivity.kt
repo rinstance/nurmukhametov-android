222bc52a3edd3ba9442052ed8b8e82413f7c571e
rinstance <rinat.nur.93@mail.ru>
2020-09-27 20:22:16 +0300

    init

--- app/src/main/java/com/example/myapplication/RegisterActivity.kt
+++ app/src/main/java/com/example/myapplication/RegisterActivity.kt
@@ -12,6 +12,7 @@ class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
     }
 
     fun click(view: View) {
